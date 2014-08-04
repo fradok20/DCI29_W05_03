92771fede99bed089a7a1ca5e4a29c100e0f5328
frado <frado@DCIPC>
2014-08-04 12:54:21 -0600

    not yet comments

---
+++ src/dci29/w05/pkg03/DCI29W0503.java
@@ -0,0 +1,59 @@
+/*******************************************************************************
+                        Number addition GUI Exercise 
+ *******************************************************************************
+ *
+ * FileName:     DemoClassCollaboration.java
+ * Dependencies: java.sql.Connection
+ *               javax.sql.DriverManager
+ *               javax.sql.SQLException
+ * 
+ * Processor:    Java Virtual Machine
+ * Compiler:     JDK 7
+ * Company:      DCI Distributing
+ *
+ * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ * Author: Adolfo Ortiz Rodriguez
+ *
+ * Created on July 04, 2014, 02:40 PM
+ ******************************************************************************/
+
+package dci29.w05.pkg03;
+
+import Front_End_GUI.Login;
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author frado
+ */
+public class DCI29W0503 {
+
+    static String password_global;
+    static String user_global;
+    
+    public static void main(String[] args) {
+        
+        
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                ;
+            }
+        });
+        
+        //loging
+        
+        
+        //add user
+        //edit table
+        //other
+        //modbus features
+        //test
+        
+        
+        
+        
+        
+        // TODO code application logic here
+    }
+    
+}
