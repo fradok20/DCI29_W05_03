8fd5304c80a742e225cccd1f0319279076533958
frado <frado@DCIPC>
2014-08-04 15:56:37 -0600

    not yet comments

--- src/dci29/w05/pkg03/DCI29W0503.java
+++ src/dci29/w05/pkg03/DCI29W0503.java
@@ -40,7 +40,7 @@ public class DCI29W0503 {
         System.out.println("123");
         System.err.println("Faults");
         System.out.println("Wiu! Wiu! Wiu! Wiu! Wiu! ");
-        System.out.println("Ta bien chillon  ");
+        System.out.println("Ta bien chillo");
         
         //Si ni la llegas
         //loging
